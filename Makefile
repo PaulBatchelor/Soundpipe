95bd170228393457a79533b4bf3edf464e922c31
Paul <foo>
2015-06-02 19:01:44 -0400

    created initial alsa example that works. not confirmed to work on a pi yet...

--- Makefile
+++ Makefile
@@ -9,8 +9,8 @@ MODULES=base event noise tone moogladder butter revsc ftbl osc posc3 randi \
 #CFLAGS += -ljack
 
 #RPi Module
-#MODULES += pi
-#CFLAGS += lalsa
+MODULES += rpi
+CFLAGS += -lasound
 
 MPATHS=$(addprefix m/, $(addsuffix .o, $(MODULES)))
 HPATHS=$(addprefix h/, $(addsuffix .h, $(MODULES)))
--- m/rpi.c
+++ m/rpi.c
@@ -1,15 +1,38 @@
 #include <stdlib.h>
-#include <aaio.h>
-#include "soundrpipe.h"
+#include "alsa/asoundlib.h"
+#include "soundpipe.h"
 
-static int sp_rpi_cb(){
-    return 0;
-}
 void sp_rpi_shutdown (void *arg){
 	exit (1);
 }
 
 int sp_rpi_process(sp_data *sp, void *ud, void (*callback)(sp_data *, void *)){
-    getch();
+
+    int err;
+    unsigned int i;
+    SPFLOAT buf[SP_BUFSIZE];
+    snd_pcm_t *handle;
+	if ((err = snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
+		printf("Playback open error: %s\n", snd_strerror(err));
+		exit(EXIT_FAILURE);
+	}
+	if ((err = snd_pcm_set_params(handle,
+	                              SND_PCM_FORMAT_FLOAT,
+	                              SND_PCM_ACCESS_RW_INTERLEAVED,
+	                              1,
+	                              sp->sr,
+	                              1,
+	                              500000)) < 0) {	/* 0.5sec */
+		printf("Playback open error: %s\n", snd_strerror(err));
+		exit(EXIT_FAILURE);
+	}
+    while(1) {
+        for(i = 0; i < SP_BUFSIZE; i++){
+            callback(sp, ud);
+            buf[i] = sp->out;
+        }
+        snd_pcm_writei(handle, buf, SP_BUFSIZE);
+    }
+	snd_pcm_close(handle);
     return SP_OK;
 }
