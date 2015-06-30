21c42ee1f9afe94537d89df95092ac2a3ee33ce5
Paul <foo>
2015-06-29 23:11:33 -0400

    code review for clip.

--- examples/ex_clip.c
+++ examples/ex_clip.c
@@ -6,41 +6,32 @@ typedef struct {
    sp_ftbl *ft;
    sp_osc *osc;
    sp_clip *clp;
+   sp_moogladder *filt;
 } udata;
 
 void write_clip(sp_data *sp, void *ud) {
     udata *udp = ud;
-    SPFLOAT out, osc;
+    SPFLOAT out, osc, filt;
 
-    //if pos is 0 - 88200, no clip
-    if(sp->pos < 88200) {
+    if(sp->pos < (44100 * 2)) {
         sp_osc_compute(sp, udp->osc, NULL, &osc);
-        sp->out[0] = osc;
-        
-    }
-    //if pos is 88200 - 44100 *3, clip method 0
-    if(sp->pos >= 88200 && sp->pos < (44100 * 3)) {
+        sp_moogladder_compute(sp, udp->filt, &osc, &filt);
+        sp->out[0] = filt;
+        return;
+    } else if(sp->pos >= (44100 * 2) && sp->pos < (44100 * 3)) {
         udp->clp->meth = 0;
-        sp_osc_compute(sp, udp->osc, NULL, &osc);
-        sp_clip_compute(sp, udp->clp, &osc, &out);
-        sp->out[0] = out;
-    }
-    //if pos is 44100 * 3 - 44100 * 4, clip method 1
-    if(sp->pos >= (44100 * 3) && sp->pos < (44100 * 4)) {
+    } else if(sp->pos >= (44100 * 3) && sp->pos < (44100 * 4)) {
         udp->clp->meth = 1;
         udp->clp->lim = 0.2;
-        sp_osc_compute(sp, udp->osc, NULL, &osc);
-        sp_clip_compute(sp, udp->clp, &osc, &out);
-        sp->out[0] = out;
-    }
-    //if pos is 44100 * 4 - 44100 * 5, clip method 2
-    if(sp->pos >= (44100 * 4) && sp->pos < (44100 * 5)) {
+    } else {
         udp->clp->meth = 2;
-        udp->clp->lim = 0.2;
-        sp_osc_compute(sp, udp->osc, NULL, &osc);
-        sp_clip_compute(sp, udp->clp, &osc, &out);
-        sp->out[0] = out * 0.5;
+        udp->clp->lim = 0.03;
     }
+
+    sp_osc_compute(sp, udp->osc, NULL, &osc);
+    sp_moogladder_compute(sp, udp->filt, &osc, &filt);
+    sp_clip_compute(sp, udp->clp, &filt, &out);
+    sp->out[0] = out;
 }
 
 int main() {
@@ -50,17 +41,22 @@ int main() {
     sp_clip_create(&ud.clp);
     sp_osc_create(&ud.osc);
     sp_ftbl_create(sp, &ud.ft, 4096);
+    sp_moogladder_create(&ud.filt);
 
     sp_gen_line(sp, ud.ft, "1 4096 -1");
     sp_osc_init(sp, ud.osc, ud.ft);
     ud.osc->freq = sp_midi2cps(48);
     sp_clip_init(sp, ud.clp, 0, 0.5);
+    sp_moogladder_init(sp, ud.filt);
+    ud.filt->freq = 1000; 
+    ud.filt->res = 0.8;
     sp->len = 44100 * 5;
     sp_process(sp, &ud, write_clip);
 
     sp_osc_destroy(&ud.osc);
     sp_clip_destroy(&ud.clp);
     sp_ftbl_destroy(&ud.ft);
+    sp_moogladder_destroy(&ud.filt);
 
     sp_destroy(&sp);
     return 0;
