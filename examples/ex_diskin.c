4d6f09fedf77d714709b4f8aa9479fd9041d47e2
paulbatchelor <thisispaulbatchelor@gmail.com>
2015-08-02 21:43:29 -0400

    added diskin

---
+++ examples/ex_diskin.c
@@ -0,0 +1,45 @@
+/*
+ * This is a dummy example.
+ * Please implement a small and simple working example of your module, and then
+ * remove this header.
+ * Don't be clever.
+ * Bonus points for musicality. 
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "soundpipe.h"
+
+typedef struct {
+    sp_diskin *diskin;
+    sp_osc *osc;
+    sp_ftbl *ft; 
+} UserData;
+
+void process(sp_data *sp, void *udata) {
+    UserData *ud = udata;
+    SPFLOAT diskin = 0;
+    sp_diskin_compute(sp, ud->diskin, NULL, &diskin);
+    sp->out[0] = diskin;
+}
+
+int main() {
+    srand(1234567);
+    UserData ud;
+    sp_data *sp;
+    sp_create(&sp);
+
+    sp_diskin_create(&ud.diskin);
+
+    sp_diskin_init(sp, ud.diskin, "oneart.wav");
+
+    sp->len = 44100 * 5;
+    sp_process(sp, &ud, process);
+
+    sp_diskin_destroy(&ud.diskin);
+
+    sp_destroy(&sp);
+    return 0;
+}
