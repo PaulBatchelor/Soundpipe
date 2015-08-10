8d7853b50a654c20a13ea2e71e7757b6cf0285e5
Paul Batchelor <thisispaulbatchelor@gmail.com>
2015-08-10 13:45:29 -0400

    reverse tweak

--- modules/data/reverse.lua
+++ modules/data/reverse.lua
@@ -1,43 +1,43 @@
 sptbl["reverse"] = {
 
-	files = { 
+	files = {
 	    module = "reverse.c",
 	    header = "reverse.h",
 	    example = "ex_reverse.c",
 	},
-	
+
 	func = {
 	    create = "sp_reverse_create",
 	    destroy = "sp_reverse_destroy",
 	    init = "sp_reverse_init",
 	    compute = "sp_reverse_compute",
 	},
-	
+
 	params = {
 	    mandatory = {
 	        {
 	            name = "delay",
-	            type = "sp_ftbl *",
+	            type = "SPFLOAT",
 	            description = "Delay time in seconds.",
 	            default = "1.0"
 	        }
 	    }
 	},
-	
+
 	modtype = "module",
-	
-	description = [[Reverse will store a signal inside a buffer and play it back reversed.]], 
-	
+
+	description = [[Reverse will store a signal inside a buffer and play it back reversed.]],
+
 	ninputs = 1,
 	noutputs = 1,
-	
-	inputs = { 
+
+	inputs = {
 	    {
 	        name = "input",
 	        description = "Signal input."
 	    }
 	},
-	
+
 	outputs = {
 	    {
 	        name = "out",
