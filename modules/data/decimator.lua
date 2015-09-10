cd31a5a1955a6f6bc6435641b77347a127e1332b
Paul Batchelor <thisispaulbatchelor@gmail.com>
2015-09-10 16:34:54 -0700

    created decimator

---
+++ modules/data/decimator.lua
@@ -0,0 +1,54 @@
+sptbl["decimator"] = {
+
+    files = { 
+        module = "decimator.c",
+        header = "decimator.h",
+        example = "ex_decimator.c",
+    },
+    
+    func = {
+        create = "sp_decimator_create",
+        destroy = "sp_decimator_destroy",
+        init = "sp_decimator_init",
+        compute = "sp_decimator_compute",
+    },
+    
+    params = {
+        optional = {
+            {
+                name = "bit",
+                type = "uint32_t",
+                description = "Bitdepth",
+                default = 8
+            },
+            {
+                name = "srate",
+                type = "SPFLOAT",
+                description ="Sampling rate.",
+                default = 10000
+            },
+        }
+    },
+    
+    modtype = "module",
+    
+    description = [[Otherwise known as a "bitcrusher", Decimator will digitially degrade a signal. ]], 
+    
+    ninputs = 1,
+    noutputs = 1,
+    
+    inputs = { 
+        {
+            name = "input",
+            description = "Signal input."
+        },
+    },
+    
+    outputs = {
+        {
+            name = "out",
+            description = "Signal output"
+        },
+    }
+
+}
