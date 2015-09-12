93a36c4c1bb0f5dd3b7e2837355382d3167efd88
Aurelius Prochazka <aure@aure.com>
2015-09-11 22:42:32 -0700

    Improved pan documentation

--- modules/data/pan2.lua
+++ modules/data/pan2.lua
@@ -1,27 +1,27 @@
 sptbl["pan2"] = {
 
-    files = { 
+    files = {
         module = "pan2.c",
         header = "pan2.h",
         example = "ex_pan2.c",
     },
-    
+
     func = {
         create = "sp_pan2_create",
         destroy = "sp_pan2_destroy",
         init = "sp_pan2_init",
         compute = "sp_pan2_compute",
     },
-    
+
     params = {
         mandatory = {
         },
-    
+
         optional = {
             {
                 name = "type",
                 type = "uint32_t",
-                description = [[Panning type. 0 = equal power, 1 = square root, 2 = linear, 
+                description = [[Panning type. 0 = equal power, 1 = square root, 2 = linear,
 3 = alternative equal power. Values outside this range will wrap. ]],
                 default = 0
             },
@@ -33,30 +33,14 @@ sptbl["pan2"] = {
             },
         }
     },
-    
-    modtype = "module",
-    
-    description = [[This is a description of the entire module. This is not a real module. This description should be a comprehensive sumary of what this function does. 
-    
-Inside the Lua table, this is expressed as a multiline string, however it does not adhere to the tradtional 80 column rule found in programming. 
 
-Write as much text as needed here...
-]], 
-    
+    modtype = "module",
+
+    description = [[Panner]],
+
     ninputs = 1,
     noutputs = 2,
-    
-    inputs = { 
-        {
-            name = "clock",
-            description = "this is the clock source for a made up plugin."
-        },
-        {
-            name = "input",
-            description = "this is the audio input for a made up plugin."
-        },
-    },
-    
+
     outputs = {
         {
             name = "out_left",
