f05e8742651df90e1cb20b0a942de36167b4d380
Aurelius Prochazka <aure@aure.com>
2015-09-13 22:23:44 -0700

    The smallest of edits to get the index page aligned

--- util/gen_index.lua
+++ util/gen_index.lua
@@ -7,11 +7,12 @@ io.write("<title>Soundpipe</title>\n")
 io.write("<link rel=\"stylesheet\" href=\"css/skeleton.css\">\n")
 io.write("<link rel=\"stylesheet\" href=\"css/normalize.css\">\n")
 io.write("</head>\n")
+io.write("<body>\n<div class=\"container\">\n")
 io.write("<h1>Soundpipe Modules</h1>\n")
 for line in file:lines() do
 	io.write("<div class=\"row\">\n")
 	io.write(string.format("<a href=\"%s.html\">%s</a>\n", line, line))
 	io.write("</div>\n")
-end 
+end
 
-io.write("</html>\n")
+io.write("</div></body></html>\n")
