5c73613edac4abffb1e21f45078e13b2aaf3c2b2
Paul Batchelor <thisispaulbatchelor@gmail.com>
2015-09-15 12:39:52 -0700

    gen_index can now load access lua metadata

--- util/gen_index.lua
+++ util/gen_index.lua
@@ -1,5 +1,6 @@
 #!/usr/bin/env lua
 
+
 file = io.open(arg[1])
 io.write("<html>\n")
 io.write("<head>\n")
@@ -10,9 +11,15 @@ io.write("</head>\n")
 io.write("<body>\n<div class=\"container\">\n")
 io.write("<h1>Soundpipe Modules</h1>\n")
 for line in file:lines() do
+    sptbl = {}
+    dofile(string.format("modules/data/%s.lua", line))
 	io.write("<div class=\"row\">\n")
 	io.write(string.format("<a href=\"%s.html\">%s</a>\n", line, line))
 	io.write("</div>\n")
+    -- Prints description
+	-- io.write("<div class=\"row\">\n")
+	-- io.write(string.format("%s\n", sptbl[line].description))
+	-- io.write("</div>\n")
 end
 
 io.write("</div></body></html>\n")
