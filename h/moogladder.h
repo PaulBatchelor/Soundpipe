1c0282dffe46f045a1c749d2f8fafeb545dd156f
Aurelius Prochazka <aure@aure.com>
2015-09-25 22:20:58 -0700

    Indentation clean up

--- h/moogladder.h
+++ h/moogladder.h
@@ -1,14 +1,14 @@
 typedef struct {
-    SPFLOAT   freq;
-    SPFLOAT   res;
-    SPFLOAT   istor;
+    SPFLOAT freq;
+    SPFLOAT res;
+    SPFLOAT istor;
 
-    SPFLOAT  delay[6];
-    SPFLOAT  tanhstg[3];
-    SPFLOAT   oldfreq;
-    SPFLOAT   oldres;
-    SPFLOAT  oldacr;
-    SPFLOAT  oldtune;
+    SPFLOAT delay[6];
+    SPFLOAT tanhstg[3];
+    SPFLOAT oldfreq;
+    SPFLOAT oldres;
+    SPFLOAT oldacr;
+    SPFLOAT oldtune;
 } sp_moogladder;
 
 int sp_moogladder_create(sp_moogladder **t);
