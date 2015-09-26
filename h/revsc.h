c64f31c0fc5204ddb59b34a26aac3a3c62a193c0
Aurelius Prochazka <aure@aure.com>
2015-09-26 02:01:35 -0700

    More spacing

--- h/revsc.h
+++ h/revsc.h
@@ -3,16 +3,16 @@ size_t size;
 void *auxp;
 }auxData;
 typedef struct {
-    int         writePos;
-    int         bufferSize;
-    int         readPos;
-    int         readPosFrac;
-    int         readPosFrac_inc;
-    int         dummy;
-    int         seedVal;
-    int         randLine_cnt;
-    SPFLOAT      filterState;
-    SPFLOAT       *buf;
+    int     writePos;
+    int     bufferSize;
+    int     readPos;
+    int     readPosFrac;
+    int     readPosFrac_inc;
+    int     dummy;
+    int     seedVal;
+    int     randLine_cnt;
+    SPFLOAT filterState;
+    SPFLOAT *buf;
 } sp_revsc_dl;
 
 typedef struct  {
@@ -27,6 +27,6 @@ typedef struct  {
 } sp_revsc;
 
 int sp_revsc_create(sp_revsc **p);
-int sp_revsc_destroy(sp_revsc **p); 
+int sp_revsc_destroy(sp_revsc **p);
 int sp_revsc_init(sp_data *sp, sp_revsc *p);
 int sp_revsc_compute(sp_data *sp, sp_revsc *p, SPFLOAT *in1, SPFLOAT *in2, SPFLOAT *out1, SPFLOAT *out2);
