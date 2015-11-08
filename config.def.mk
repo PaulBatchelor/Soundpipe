b4cd269b69567520ccc96e0b2138fb62065b3dfd
Paul Batchelor <thisispaulbatchelor@gmail.com>
2015-11-07 18:01:42 -0800

    header file blues

--- config.def.mk
+++ config.def.mk
@@ -38,9 +38,6 @@ include lib/fft/Makefile
 #MODULES += padsynth
 #CFLAGS += -lfftw3
 
-# If you are on OSX, you may need this
-# CFLAGS += -I /usr/local/include -L /usr/local/include
-
 # Build dynamic library. Needed for Sporth chugin
 # CFLAGS += -shared -fPIC
 # BUILD_DYNAMIC=1
