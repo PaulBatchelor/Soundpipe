fecc7a8f1b2ba298cbfecf9cc3d75bc89a4bec5c
Nicholas Arner <nicholasarner@gmail.com>
2015-12-05 14:32:03 -0500

    Added randh test.

--- test/all_tests.h
+++ test/all_tests.h
@@ -57,4 +57,5 @@ TEST(t_gen_vals, "gen_vals", "b40825a6ee05f1219b87f3b30d5f1331")
 TEST(t_gen_xline, "gen_xline", "5c6050443a98326723e1809b67b73a9f")
 TEST(t_gen_line, "gen_line", "9eb3fdbed1598e756ed3b2ce7df03889")
 TEST(t_expon, "expon", "4e7c83be557f938e282c854ba04d4427")
+TEST(t_randh, "randh", "2f47093ad75fa1c882a30e8dec8ab220")
 
