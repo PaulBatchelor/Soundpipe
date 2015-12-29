382db7b2861bc52ddd47082792604976e3c80dcf
Paul Batchelor <pbatch@ccrma.stanford.edu>
2015-12-29 14:23:24 -0500

    conv: updated docs

--- modules/data/conv.lua
+++ modules/data/conv.lua
@@ -11,92 +11,49 @@ sptbl["conv"] = {
         destroy = "sp_conv_destroy",
         init = "sp_conv_init",
         compute = "sp_conv_compute",
-        other = {
-            sp_conv_set = {
-                description = "conv_set description goes here.",
-                args = {
-                    {
-                        name = "var1",
-                        type = "SPFLOAT",
-                        description = "This is the first parameter",
-                        default = 1.5
-                    },
-                    {
-                        name = "var2",
-                        type = "SPFLOAT",
-                        description = "This is the second parameter",
-                        default = 1.5
-                    }
-                }
-            }
-        }
     },
 
     params = {
         mandatory = {
             {
-                name = "bar",
+                name = "ft",
                 type = "sp_ftbl *",
-                description = "This is a mandatory table value. It does not have a default value, so we set it to 'N/A'. Any that does not or cannot have a default value should set this default value to 'N/A'.",
+                description = "Ftable used as the impulse response. ",
                 default = "N/A"
             },
             {
-                name = "bar2",
+                name = "iPartLen",
                 type = "SPFLOAT",
-                description = "This is a mandatory parameter. In soundpipe, users will always need to specify this value, but a default value has been giving in case it is needed to write more complicated engines in the future.",
-                default = 123
+                description = [[Partition length (in samples). 
+Must be a power of 2. Lower values will add less latency, at the cost 
+of requiring more CPU power. 
+]],
+                default = 2048
             }
         },
 
-        optional = {
-            {
-                name = "blah_1",
-                type = "SPFLOAT",
-                description = "This is an optional parameter. These values are always set to a value by default, and can be set after the init function has been called.",
-                default = 1.5
-            },
-            {
-                name = "blah_2",
-                type = "SPFLOAT",
-                description ="This is yet another optional parameter...",
-                default = 456.7
-            },
-        }
     },
 
     modtype = "module",
 
-    description = [[A short title describing the module
+    description = [[Partioned convolution. 
+This module will perform partitioned convolution on an input signal using
+an ftable as an impulse response.]],
 
-    This is a description of the entire module.  This is not a real module. This description should be a comprehensive sumary of what this function does.
-
-Inside the Lua table, this is expressed as a multiline string, however it does not adhere to the tradtional 80 column rule found in programming.
-
-Write as much text as needed here...
-]],
-
-    ninputs = 2,
-    noutputs = 2,
+    ninputs = 1,
+    noutputs = 1,
 
     inputs = {
         {
-            name = "clock",
-            description = "this is the clock source for a made up plugin."
-        },
-        {
             name = "input",
-            description = "this is the audio input for a made up plugin."
+            description = "Signal input to be convolved."
         },
     },
 
     outputs = {
         {
-            name = "out_left",
-            description = "Stereo left output for conv."
-        },
-        {
-            name = "out_right",
-            description = "Stereo right output for conv."
+            name = "out",
+            description = "Signal output."
         },
     }
 
