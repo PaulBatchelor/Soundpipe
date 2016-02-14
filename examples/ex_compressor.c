170fa42ecb4ea04fd128f2e2382e37429cda4152
Paul Batchelor <pbatch@ccrma.stanford.edu>
2016-02-13 22:35:07 -0500

    added compressor example

--- examples/ex_compressor.c
+++ examples/ex_compressor.c
@@ -14,15 +14,14 @@
 
 typedef struct {
     sp_compressor *compressor;
-    sp_osc *osc;
-    sp_ftbl *ft; 
+    sp_diskin *diskin;
 } UserData;
 
 void process(sp_data *sp, void *udata) {
     UserData *ud = udata;
-    SPFLOAT osc = 0, compressor = 0;
-    sp_osc_compute(sp, ud->osc, NULL, &osc);
-    sp_compressor_compute(sp, ud->compressor, &osc, &compressor);
+    SPFLOAT diskin = 0, compressor = 0;
+    sp_diskin_compute(sp, ud->diskin, NULL, &diskin);
+    sp_compressor_compute(sp, ud->compressor, &diskin, &compressor);
     sp->out[0] = compressor;
 }
 
@@ -33,19 +32,20 @@ int main() {
     sp_create(&sp);
 
     sp_compressor_create(&ud.compressor);
-    sp_osc_create(&ud.osc);
-    sp_ftbl_create(sp, &ud.ft, 2048);
+    sp_diskin_create(&ud.diskin);
 
     sp_compressor_init(sp, ud.compressor);
-    sp_gen_sine(sp, ud.ft);
-    sp_osc_init(sp, ud.osc, ud.ft, 0);
+    *ud.compressor->ratio = 4;
+    *ud.compressor->thresh = -30;
+    *ud.compressor->atk = 0.2;
+    *ud.compressor->rel = 0.2;
+    sp_diskin_init(sp, ud.diskin, "oneart.wav");
 
     sp->len = 44100 * 5;
     sp_process(sp, &ud, process);
 
     sp_compressor_destroy(&ud.compressor);
-    sp_ftbl_destroy(&ud.ft);
-    sp_osc_destroy(&ud.osc);
+    sp_diskin_destroy(&ud.diskin);
 
     sp_destroy(&sp);
     return 0;
