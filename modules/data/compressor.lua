7467da87d9ce0932285070643f637a6aec5be413
Paul Batchelor <pbatch@ccrma.stanford.edu>
2016-02-13 22:39:17 -0500

    added documentation

--- modules/data/compressor.lua
+++ modules/data/compressor.lua
@@ -11,92 +11,58 @@ sptbl["compressor"] = {
         destroy = "sp_compressor_destroy",
         init = "sp_compressor_init",
         compute = "sp_compressor_compute",
-        other = {
-            sp_compressor_set = {
-                description = "compressor_set description goes here.",
-                args = {
-                    {
-                        name = "var1",
-                        type = "SPFLOAT",
-                        description = "This is the first parameter",
-                        default = 1.5
-                    },
-                    {
-                        name = "var2",
-                        type = "SPFLOAT",
-                        description = "This is the second parameter",
-                        default = 1.5
-                    }
-                }
-            }
-        }
     },
 
     params = {
         mandatory = {
-            {
-                name = "bar",
-                type = "sp_ftbl *",
-                description = "This is a mandatory table value. It does not have a default value, so we set it to 'N/A'. Any that does not or cannot have a default value should set this default value to 'N/A'.",
-                default = "N/A"
-            },
-            {
-                name = "bar2",
-                type = "SPFLOAT",
-                description = "This is a mandatory parameter. In soundpipe, users will always need to specify this value, but a default value has been giving in case it is needed to write more complicated engines in the future.",
-                default = 123
-            }
         },
 
         optional = {
             {
-                name = "blah_1",
+                name = "ratio",
                 type = "SPFLOAT",
-                description = "This is an optional parameter. These values are always set to a value by default, and can be set after the init function has been called.",
-                default = 1.5
+                description = "Ratio to compress with, a value > 1 will compress",
+                default = 1
             },
             {
-                name = "blah_2",
+                name = "thresh",
                 type = "SPFLOAT",
-                description ="This is yet another optional parameter...",
-                default = 456.7
+                description ="Threshold (in dB) 0 = max",
+                default = 0
+            },
+            {
+                name = "atk",
+                type = "SPFLOAT",
+                description ="Compressor attack",
+                default = 0.1
+            },
+            {
+                name = "rel",
+                type = "SPFLOAT",
+                description ="Compressor release",
+                default = 0.1
             },
         }
     },
 
     modtype = "module",
 
-    description = [[A short title describing the module
+    description = [[Compressor]],
 
-    This is a description of the entire module.  This is not a real module. This description should be a comprehensive sumary of what this function does.
-
-Inside the Lua table, this is expressed as a multiline string, however it does not adhere to the tradtional 80 column rule found in programming.
-
-Write as much text as needed here...
-]],
-
-    ninputs = 2,
-    noutputs = 2,
+    ninputs = 1,
+    noutputs = 1,
 
     inputs = {
         {
-            name = "clock",
-            description = "this is the clock source for a made up plugin."
-        },
-        {
             name = "input",
-            description = "this is the audio input for a made up plugin."
+            description = "Signal input."
         },
     },
 
     outputs = {
         {
-            name = "out_left",
-            description = "Stereo left output for compressor."
-        },
-        {
-            name = "out_right",
-            description = "Stereo right output for compressor."
+            name = "out",
+            description = "Signal output."
         },
     }
 
