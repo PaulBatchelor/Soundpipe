12daa1e5c81e001df793c177e200e15ed0ea5265
Paul Batchelor <pbatch@ccrma.stanford.edu>
2016-02-13 22:25:32 -0500

    compress: faust DSP code imported

--- h/compressor.h
+++ h/compressor.h
@@ -1,5 +1,11 @@
 typedef struct {
-    SPFLOAT bar;
+    void *faust;
+    int argpos;
+    SPFLOAT *args[4];
+    SPFLOAT *ratio;
+    SPFLOAT *thresh;
+    SPFLOAT *atk;
+    SPFLOAT *rel;
 } sp_compressor;
 
 int sp_compressor_create(sp_compressor **p);
