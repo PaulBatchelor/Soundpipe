0ec8d2c7676fd21aca4d4415bbb4aac3bf10d05b
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-04-23 20:15:38 -0700

    created paulstretch docs

--- modules/data/paulstretch.lua
+++ modules/data/paulstretch.lua
@@ -11,92 +11,52 @@ sptbl["paulstretch"] = {
         destroy = "sp_paulstretch_destroy",
         init = "sp_paulstretch_init",
         compute = "sp_paulstretch_compute",
-        other = {
-            sp_paulstretch_set = {
-                description = "paulstretch_set description goes here.",
-                args = {
-                    {
-                        name = "var1",
-                        type = "SPFLOAT",
-                        description = "This is the first parameter",
-                        default = 1.5
-                    },
-                    {
-                        name = "var2",
-                        type = "SPFLOAT",
-                        description = "This is the second parameter",
-                        default = 1.5
-                    }
-                }
-            }
-        }
     },
 
     params = {
         mandatory = {
             {
-                name = "bar",
+                name = "ft",
                 type = "sp_ftbl *",
-                description = "This is a mandatory table value. It does not have a default value, so we set it to 'N/A'. Any that does not or cannot have a default value should set this default value to 'N/A'.",
+                description = "ftable containing audio data",
                 default = "N/A"
             },
             {
-                name = "bar2",
+                name = "windowsize",
                 type = "SPFLOAT",
-                description = "This is a mandatory parameter. In soundpipe, users will always need to specify this value, but a default value has been giving in case it is needed to write more complicated engines in the future.",
-                default = 123
+                description = "Window size, in seconds.",
+                default = 10
             }
         },
 
         optional = {
             {
-                name = "blah_1",
+                name = "stretch",
                 type = "SPFLOAT",
-                description = "This is an optional parameter. These values are always set to a value by default, and can be set after the init function has been called.",
-                default = 1.5
-            },
-            {
-                name = "blah_2",
-                type = "SPFLOAT",
-                description ="This is yet another optional parameter...",
-                default = 456.7
+                description = "Stretch factor, 1.0 is no stretch.",
+                default = 10
             },
         }
     },
 
     modtype = "module",
 
-    description = [[A short title describing the module
-
-    This is a description of the entire module.  This is not a real module. This description should be a comprehensive sumary of what this function does.
-
-Inside the Lua table, this is expressed as a multiline string, however it does not adhere to the tradtional 80 column rule found in programming.
-
-Write as much text as needed here...
+    description = [[An extreme time-stretching algorithm by Paul Nasca Octavian
+This is an implementation of the popular paulstretch algorithm used for time
+stretching an audio signal to create ambient textures. Ideally, this algorithm 
+is best used for stretching signals by very very long amounts. 
 ]],
 
-    ninputs = 2,
-    noutputs = 2,
+    ninputs = 0,
+    noutputs = 1,
 
     inputs = {
-        {
-            name = "clock",
-            description = "this is the clock source for a made up plugin."
-        },
-        {
-            name = "input",
-            description = "this is the audio input for a made up plugin."
-        },
     },
 
     outputs = {
         {
-            name = "out_left",
-            description = "Stereo left output for paulstretch."
-        },
-        {
-            name = "out_right",
-            description = "Stereo right output for paulstretch."
+            name = "out",
+            description = "Signal output."
         },
     }
 
