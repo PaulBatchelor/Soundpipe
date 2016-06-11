dc6975fcc570ba79c1b229845e00d7e3e2499c96
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-06-11 09:48:01 -0700

    added hilbert example

--- examples/ex_hilbert.c
+++ examples/ex_hilbert.c
@@ -1,12 +1,3 @@
-/*
- * This is a dummy example.
- * Please implement a small and simple working example of your module, and then
- * remove this header.
- * Don't be clever.
- * Bonus points for musicality. 
- *
- */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -14,16 +5,23 @@
 
 typedef struct {
     sp_hilbert *hilbert;
-    sp_osc *osc;
+    sp_osc *cos, *sin;
     sp_ftbl *ft; 
+    sp_diskin *diskin;
 } UserData;
 
 void process(sp_data *sp, void *udata) {
     UserData *ud = udata;
-    SPFLOAT osc = 0, hilbert = 0;
-    sp_osc_compute(sp, ud->osc, NULL, &osc);
-    sp_hilbert_compute(sp, ud->hilbert, &osc, &hilbert);
-    sp->out[0] = hilbert;
+    SPFLOAT disk= 0;
+    SPFLOAT real = 0, imag = 0;
+    SPFLOAT diskin = 0;
+    SPFLOAT sin = 0, cos = 0;
+
+    sp_diskin_compute(sp, ud->diskin, NULL, &diskin);
+    sp_osc_compute(sp, ud->sin, NULL, &sin);
+    sp_osc_compute(sp, ud->cos, NULL, &cos);
+    sp_hilbert_compute(sp, ud->hilbert, &diskin, &real, &imag);
+    sp->out[0] = ((cos * real) + (sin * real)) * 0.7;
 }
 
 int main() {
@@ -33,19 +31,27 @@ int main() {
     sp_create(&sp);
 
     sp_hilbert_create(&ud.hilbert);
-    sp_osc_create(&ud.osc);
-    sp_ftbl_create(sp, &ud.ft, 2048);
+    sp_osc_create(&ud.sin);
+    sp_osc_create(&ud.cos);
+    sp_diskin_create(&ud.diskin);
+    sp_ftbl_create(sp, &ud.ft, 8192);
 
     sp_hilbert_init(sp, ud.hilbert);
     sp_gen_sine(sp, ud.ft);
-    sp_osc_init(sp, ud.osc, ud.ft, 0);
+    sp_osc_init(sp, ud.sin, ud.ft, 0);
+    sp_osc_init(sp, ud.cos, ud.ft, 0.25);
+    ud.sin->freq = 1000;
+    ud.cos->freq = 1000;
+    sp_diskin_init(sp, ud.diskin, "oneart.wav");
 
     sp->len = 44100 * 5;
     sp_process(sp, &ud, process);
 
     sp_hilbert_destroy(&ud.hilbert);
     sp_ftbl_destroy(&ud.ft);
-    sp_osc_destroy(&ud.osc);
+    sp_osc_destroy(&ud.sin);
+    sp_osc_destroy(&ud.cos);
+    sp_diskin_destroy(&ud.diskin);
 
     sp_destroy(&sp);
     return 0;
