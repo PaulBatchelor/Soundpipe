9510553f7c6db72f53ea01dbd785bb5372d2f229
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-06-13 18:37:07 -0700

    tseg example added

--- examples/ex_tseg.c
+++ examples/ex_tseg.c
@@ -21,9 +21,11 @@ typedef struct {
 void process(sp_data *sp, void *udata) {
     UserData *ud = udata;
     SPFLOAT osc = 0, tseg = 0;
+    SPFLOAT trig = (sp->pos == 0);
+    sp_tseg_compute(sp, ud->tseg, &trig, &tseg);
+    ud->osc->freq = 100 + (tseg * 1000);
     sp_osc_compute(sp, ud->osc, NULL, &osc);
-    sp_tseg_compute(sp, ud->tseg, &osc, &tseg);
-    sp->out[0] = tseg;
+    sp->out[0] = osc;
 }
 
 int main() {
@@ -36,7 +38,11 @@ int main() {
     sp_osc_create(&ud.osc);
     sp_ftbl_create(sp, &ud.ft, 2048);
 
-    sp_tseg_init(sp, ud.tseg);
+    sp_tseg_init(sp, ud.tseg, 0.0001);
+    ud.tseg->end = 1.0;
+    ud.tseg->type = 3.0;
+    ud.tseg->dur = 4.0;
+
     sp_gen_sine(sp, ud.ft);
     sp_osc_init(sp, ud.osc, ud.ft, 0);
 
