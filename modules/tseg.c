62a4816ed3f058313526817ab587add08e86e480
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-06-13 18:37:40 -0700

    tseg example added

--- modules/tseg.c
+++ modules/tseg.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <math.h>
 #include "soundpipe.h"
 
 int sp_tseg_create(sp_tseg **p)
@@ -30,16 +31,16 @@ int sp_tseg_init(sp_data *sp, sp_tseg *p, SPFLOAT ibeg)
 int sp_tseg_compute(sp_data *sp, sp_tseg *p, SPFLOAT *in, SPFLOAT *out)
 {
     *out = p->val;
-    if(*in) {
+    if(*in != 0) {
         p->slope = 1.0 / (1.0 - exp(p->type));
         p->beg = p->val;
         p->count = 0;
         p->steps = p->dur * sp->sr;
         p->tdivnsteps = (SPFLOAT)p->type / (p->steps - 1);
     }
-    if(p->count == p->steps) {
-        *out = p->beg + (p->end- p->beg) * 
-            (1 - exp(p->count * p->tdivnsteps) * p->slope);
+    if(p->count < p->steps) {
+        *out = p->beg + (p->end - p->beg) * 
+            ((1 - exp(p->count * p->tdivnsteps)) * p->slope);
         p->val = *out;
         p->count++;
     }
