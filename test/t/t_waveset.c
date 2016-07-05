8e8d066020b59451b9388fafd0faa91fd60da10b
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-07-05 11:37:58 -0700

    added waveset test

--- test/t/t_waveset.c
+++ test/t/t_waveset.c
@@ -3,23 +3,36 @@
 #include "tap.h"
 #include "test.h"
 
+typedef struct {
+    sp_waveset *waveset;
+    sp_diskin *diskin;
+} UserData;
+
 int t_waveset(sp_test *tst, sp_data *sp, const char *hash) 
 {
     uint32_t n;
     int fail = 0;
+    SPFLOAT wav = 0, waveset = 0;
 
-    /* allocate / initialize modules here */
+    UserData ud;
+    sp_srand(sp, 1234567);
+
+    sp_waveset_create(&ud.waveset);
+    sp_diskin_create(&ud.diskin);
+
+    sp_waveset_init(sp, ud.waveset, 1.0);
+    ud.waveset->rep = 3.0;
+    sp_diskin_init(sp, ud.diskin, SAMPDIR "oneart.wav");
 
     for(n = 0; n < tst->size; n++) {
-        /* compute samples and add to test buffer */
-        sp_test_add_sample(tst, 0);
+        wav = 0; waveset = 0;
+        sp_diskin_compute(sp, ud.diskin, NULL, &wav);
+        sp_waveset_compute(sp, ud.waveset, &wav, &waveset);
+        sp_test_add_sample(tst, waveset);
     }
 
     fail = sp_test_verify(tst, hash);
 
-    /* destroy functions here */
-
     if(fail) return SP_NOT_OK;
-    /* fail by default */
-    else return SP_NOT_OK;
+    else return SP_OK;
 }
