05b973f12ea63b39216d74f4dafcb1618aac9a95
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-08-29 19:20:03 -0700

    added documentation

--- modules/data/slice.lua
+++ modules/data/slice.lua
@@ -11,88 +11,54 @@ sptbl["slice"] = {
         destroy = "sp_slice_destroy",
         init = "sp_slice_init",
         compute = "sp_slice_compute",
-        other = {
-            sp_slice_set = {
-                description = "slice_set description goes here.",
-                args = {
-                    {
-                        name = "var1",
-                        type = "SPFLOAT",
-                        description = "This is the first parameter",
-                        default = 1.5
-                    },
-                    {
-                        name = "var2",
-                        type = "SPFLOAT",
-                        description = "This is the second parameter",
-                        default = 1.5
-                    }
-                }
-            }
-        }
     },
 
     params = {
         mandatory = {
             {
-                name = "bar",
+                name = "vals",
                 type = "sp_ftbl *",
-                description = "This is a mandatory table value. It does not have a default value, so we set it to 'N/A'. Any that does not or cannot have a default value should set this default value to 'N/A'.",
+                description = "A table containing slice points, in samples",
                 default = "N/A"
             },
             {
-                name = "bar2",
-                type = "SPFLOAT",
-                description = "This is a mandatory parameter. In soundpipe, users will always need to specify this value, but a default value has been giving in case it is needed to write more complicated engines in the future.",
-                default = 123
+                name = "buf",
+                type = "sp_ftbl *",
+                description = "The buffer containing the audio samples.",
+                default = "N/A"
             }
         },
 
         optional = {
             {
-                name = "blah_1",
+                name = "id",
                 type = "SPFLOAT",
-                description = "This is an optional parameter. These values are always set to a value by default, and can be set after the init function has been called.",
-                default = 1.5
-            },
-            {
-                name = "blah_2",
-                type = "SPFLOAT",
-                description ="This is yet another optional parameter...",
-                default = 456.7
+                description = "Value position.",
+                default = 0
             },
         }
     },
 
     modtype = "module",
 
-    description = [[A short title describing the module
-
-    This is a description of the entire module.  This is not a real module. This description should be a comprehensive sumary of what this function does.
-
-Inside the Lua table, this is expressed as a multiline string, however it does not adhere to the tradtional 80 column rule found in programming.
-
-Write as much text as needed here...
+    description = [[Yet another slice-based sample player.
+This module takes in an audio buffer and a table with slice points. 
 ]],
 
-    ninputs = 2,
-    noutputs = 2,
+    ninputs = 1,
+    noutputs = 1,
 
     inputs = {
         {
-            name = "clock",
-            description = "this is the clock source for a made up plugin."
-        },
-        {
-            name = "input",
-            description = "this is the audio input for a made up plugin."
+            name = "trig",
+            description = "When non-zero, (re)triggers samples"
         },
     },
 
     outputs = {
         {
-            name = "out_left",
-            description = "Stereo left output for slice."
+            name = "out",
+            description = "Signal output."
         },
         {
             name = "out_right",
