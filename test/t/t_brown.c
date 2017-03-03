aab065571dddb6d33f93117c7e7cb85d1d7d4f4b
Paul Batchelor <thisispaulbatchelor@gmail.com>
2017-03-02 18:45:23 -0800

    added test for brown

--- test/t/t_brown.c
+++ test/t/t_brown.c
@@ -3,23 +3,30 @@
 #include "tap.h"
 #include "test.h"
 
+typedef struct {
+    sp_brown *brown;
+} UserData;
+
 int t_brown(sp_test *tst, sp_data *sp, const char *hash) 
 {
     uint32_t n;
     int fail = 0;
+    SPFLOAT brown = 0;
 
-    /* allocate / initialize modules here */
+    UserData ud;
+    sp_brown_create(&ud.brown);
+
+    sp_brown_init(sp, ud.brown);
 
     for(n = 0; n < tst->size; n++) {
-        /* compute samples and add to test buffer */
-        sp_test_add_sample(tst, 0);
+        sp_brown_compute(sp, ud.brown, NULL, &brown);
+        sp_test_add_sample(tst, brown);
     }
 
     fail = sp_test_verify(tst, hash);
-
-    /* destroy functions here */
+    
+    sp_brown_destroy(&ud.brown);
 
     if(fail) return SP_NOT_OK;
-    /* fail by default */
-    else return SP_NOT_OK;
+    else return SP_OK;
 }
