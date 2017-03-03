c574278575819d36b1ee54eb12e0741ea4fbdc7f
Paul Batchelor <thisispaulbatchelor@gmail.com>
2017-03-02 18:46:37 -0800

    cleaned up example for brown

--- examples/ex_brown.c
+++ examples/ex_brown.c
@@ -5,13 +5,11 @@
 
 typedef struct {
     sp_brown *brown;
-    sp_osc *osc;
-    sp_ftbl *ft; 
 } UserData;
 
 void process(sp_data *sp, void *udata) {
     UserData *ud = udata;
-    SPFLOAT osc = 0, brown = 0;
+    SPFLOAT brown = 0;
     sp_brown_compute(sp, ud->brown, NULL, &brown);
     sp->out[0] = brown;
 }
@@ -23,19 +21,13 @@ int main() {
     sp_srand(sp, 1234567);
 
     sp_brown_create(&ud.brown);
-    sp_osc_create(&ud.osc);
-    sp_ftbl_create(sp, &ud.ft, 2048);
 
     sp_brown_init(sp, ud.brown);
-    sp_gen_sine(sp, ud.ft);
-    sp_osc_init(sp, ud.osc, ud.ft, 0);
 
     sp->len = 44100 * 5;
     sp_process(sp, &ud, process);
 
     sp_brown_destroy(&ud.brown);
-    sp_ftbl_destroy(&ud.ft);
-    sp_osc_destroy(&ud.osc);
 
     sp_destroy(&sp);
     return 0;
