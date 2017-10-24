5e6306a074227947e7b0ecf0566c3a7c29f9a322
Paul Batchelor <thisispaulbatchelor@gmail.com>
2017-10-23 20:10:17 -0400

    lpc test uses spa

--- test/t/t_lpc.c
+++ test/t/t_lpc.c
@@ -4,7 +4,7 @@
 #include "test.h"
 
 typedef struct {
-    sp_diskin *wav;
+    sp_spa *wav;
     sp_lpc *lpc;
 } user_data;
 
@@ -20,12 +20,12 @@ int t_lpc(sp_test *tst, sp_data *sp, const char *hash)
     out = 0;
     sp_lpc_create(&dt.lpc);
     sp_lpc_init(sp, dt.lpc, 512);
-    sp_diskin_create(&dt.wav);
-    sp_diskin_init(sp, dt.wav, SAMPDIR "oneart.wav");
+    sp_spa_create(&dt.wav);
+    sp_spa_init(sp, dt.wav, SAMPDIR "oneart.spa");
 
 
     for(n = 0; n < tst->size; n++) {
-        sp_diskin_compute(sp, dt.wav, NULL, &diskin);
+        sp_spa_compute(sp, dt.wav, NULL, &diskin);
         sp_lpc_compute(sp, dt.lpc, &diskin, &out);
 
         sp_test_add_sample(tst, out);
@@ -33,7 +33,7 @@ int t_lpc(sp_test *tst, sp_data *sp, const char *hash)
 
     fail = sp_test_verify(tst, hash);
 
-    sp_diskin_destroy(&dt.wav);
+    sp_spa_destroy(&dt.wav);
     sp_lpc_destroy(&dt.lpc);
 
     if(fail) return SP_NOT_OK;
