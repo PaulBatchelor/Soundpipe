b23d9e427d972de3d62f5b1f13d01cf7adb61666
John Belmonte <john@neggie.net>
2017-12-07 11:22:32 +0900

    Fix jack example.

--- examples/config.def.mk
+++ examples/config.def.mk
@@ -134,21 +134,21 @@ ex_zitarev
 # Jack Module
 #
 #files += extra/ex_jack
-#extra/ex_jack: extra/ex_jack.c
-#	gcc -g --std=c99 -I ../h $< ../libsoundpipe.a -lsndfile -ljack -laaio -o $@ -lm
+#extra/ex_jack.bin: extra/ex_jack.c
+#	gcc -g --std=c99 -I ../h $< ../libsoundpipe.a -lsndfile -ljack -o $@ -lm
 
 # RPI Module
 #
 #files += extra/ex_rpi
-#extra/ex_rpi: extra/ex_rpi.c
+#extra/ex_rpi.bin: extra/ex_rpi.c
 #	gcc -g --std=c99 -I ../h $< ../libsoundpipe.a -lsndfile -lasound -o $@ -lm
 
 #files += extra/ex_rpi_v2
-#extra/ex_rpi_v2: extra/ex_rpi_v2.c
+#extra/ex_rpi_v2.bin: extra/ex_rpi_v2.c
 #	gcc -g --std=c99 -I ../h $< ../libsoundpipe.a -lsndfile -lasound -o $@ -lm
 
 # Padsynth Module
 #
 #files += extra/ex_padsynth
-#extra/ex_padsynth: extra/ex_padsynth.c
+#extra/ex_padsynth.bin: extra/ex_padsynth.c
 #	gcc -g --std=c99 -I ../h $< ../libsoundpipe.a -lsndfile -lfftw3 -o $@ -lm
