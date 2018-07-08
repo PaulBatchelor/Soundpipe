d5004103552b1e89618c2ac1519b6992798328c5
Paul Batchelor <thisispaulbatchelor@gmail.com>
2018-07-08 22:36:52 +0200

    segfault fixes for loadfile

--- modules/loadwav.c
+++ modules/loadwav.c
@@ -7,12 +7,15 @@ int sp_ftbl_loadwav(sp_data *sp, sp_ftbl **ft, const char *filename)
     drwav wav;
     size_t size;
     SPFLOAT *tbl;
+    sp_ftbl *ftp;
+
     drwav_init_file(&wav, filename);
     size = wav.totalSampleCount;
-    ft = malloc(sizeof(sp_ftbl));
-    (*ft)->tbl = malloc(sizeof(SPFLOAT) * (size + 1));
-    tbl = (*ft)->tbl;
-    sp_ftbl_init(sp, *ft, size);
+    *ft = malloc(sizeof(sp_ftbl));
+    ftp = *ft;
+    ftp->tbl = malloc(sizeof(SPFLOAT) * (size + 1));
+    tbl = ftp->tbl;
+    sp_ftbl_init(sp, ftp, size);
     drwav_read_f32(&wav, size, tbl);
     drwav_uninit(&wav);
     return SP_OK;
