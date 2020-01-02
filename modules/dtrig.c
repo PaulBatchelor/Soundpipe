240148d910e1fe7c8680c530722d41e6bc6016ed
Paul Batchelor <thisispaulbatchelor@gmail.com>
2020-01-02 18:12:15 -0500

    dtrig: code cleanup

--- modules/dtrig.c
+++ modules/dtrig.c
@@ -27,21 +27,21 @@ int sp_dtrig_init(sp_data *sp, sp_dtrig *p, sp_ftbl *ft)
 
 int sp_dtrig_compute(sp_data *sp, sp_dtrig *p, SPFLOAT *in, SPFLOAT *out)
 {
-    if(*in == 1.0){
+    if (*in == 1.0) {
         p->running = 1.0;
         p->pos = 0;
         p->counter = p->delay * sp->sr;
     }
-    if((p->pos < p->ft->size) && p->running){
-        if(p->counter == 0){
+    if ((p->pos < p->ft->size) && p->running) {
+        if (p->counter == 0) {
             p->counter = (uint32_t)(p->scale * p->ft->tbl[p->pos] * sp->sr - 1);
             *out = 1.0;
             p->pos++;
-            if(p->loop){
+            if (p->loop) {
                 p->pos %= p->ft->size;
             }
             return SP_OK;
-        }else{
+        } else {
             *out = 0;
             p->counter--;
             return SP_OK;
